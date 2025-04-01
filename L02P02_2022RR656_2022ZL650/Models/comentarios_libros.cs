4c4f01e882021de11cb09058dbcd084e56c44b3a
Paquix <jfzluna@gmail.com>
2025-04-01 17:43:03 -0600

    modelo cometarios libros

---
+++ L02P02_2022RR656_2022ZL650/Models/comentarios_libros.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace L02P02_2022RR656_2022ZL650.Models
+    
+{
+    public class comentarios_libros
+    {
+        [Key]
+        public int id { get; set; }
+        [Required]
+        public int id_libro { get; set; }
+        public string comentarios { get; set; }
+
+        public string usuario { get; set; }
+
+        public DateTime Created_at { get; set; }    
+
+
+    }
+}
