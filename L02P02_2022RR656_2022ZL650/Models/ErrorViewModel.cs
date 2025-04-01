aa1bf5177c8eac627b57121b241162a1b1667b50
Brandleo <brandon.repreza@catolica.edu.sv>
2025-04-01 16:49:57 -0600

    Agregar archivos de proyecto.

---
+++ L02P02_2022RR656_2022ZL650/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace L02P02_2022RR656_2022ZL650.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
